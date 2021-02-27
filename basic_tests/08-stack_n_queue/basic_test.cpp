804ac7c9b2f911c7f5859bcecf02dd156843d08e
barkeshli <sxbarkeshli@pasadena.edu>
2021-02-26 16:05:05 -0800

    stack/q basic_test: added output as comment to the end of file

--- basic_tests/08-stack_n_queue/basic_test.cpp
+++ basic_tests/08-stack_n_queue/basic_test.cpp
@@ -142,3 +142,188 @@ int main(int argc, char **argv) {
   return RUN_ALL_TESTS();
 }
 
+
+/*
+
+08-stack-queue git:(master) ✗  😊 $> tr includes 
+includes
+├── linked_list_functions
+├── node
+├── queue
+└── stack
+
+4 directories, 0 files
+08-stack-queue git:(master) ✗  😊 $> build/bin/basic_test
+
+
+----------running basic_test.cpp---------
+
+
+[==========] Running 3 tests from 1 test case.
+[----------] Global test environment set-up.
+[----------] 3 tests from TEST_FUNCTIONS
+[ RUN      ] TEST_FUNCTIONS.TestLinkedListFunctions
+Head-><-[100]-> <-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> |||
+
+
+======= testing insert =======================
+
+
+----------- A node at the middle: 50
+
+
+testing prev and next links: 
+found 50: <-[50]-> 
+before: 50: <-[60]-> 
+after: 50: <-[40]-> 
+
+
+insert_after(): 
+Head-><-[100]-> <-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[45]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> |||
+<-[45]->  was inserted
+
+
+insert_before()
+Head-><-[100]-> <-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[55]-> <-[50]-> <-[45]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> |||
+<-[55]->  was inserted
+
+
+======= testing delete =======================
+/n/n------------- deleting the last element: 
+about to delete <-[10]-> 
+deleted 10: 
+Head-><-[100]-> <-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[55]-> <-[50]-> <-[45]-> <-[40]-> <-[30]-> <-[20]-> |||
+[       OK ] TEST_FUNCTIONS.TestLinkedListFunctions (0 ms)
+[ RUN      ] TEST_FUNCTIONS.TestQueue
+queue: Queue:Head-><-[0]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> |||
+
+queue: Queue:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+
+
+it->>0
+it->>10
+it->>20
+it->>30
+it->>40
+it->>50
+it->>60
+it->>70
+it->>80
+it->>90
+
+
+popping queue: [   0]: Queue:Head-><-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping queue: [  10]: Queue:Head-><-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping queue: [  20]: Queue:Head-><-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping queue: [  30]: Queue:Head-><-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping queue: [  40]: Queue:Head-><-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping queue: [  50]: Queue:Head-><-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping queue: [  60]: Queue:Head-><-[70]-> <-[80]-> <-[90]-> |||
+
+popping queue: [  70]: Queue:Head-><-[80]-> <-[90]-> |||
+
+popping queue: [  80]: Queue:Head-><-[90]-> |||
+
+popping queue: [  90]: Queue:Head->|||
+
+
+
+Iterator is_null(): true
+
+----------------------- 
+
+[       OK ] TEST_FUNCTIONS.TestQueue (0 ms)
+[ RUN      ] TEST_FUNCTIONS.TestStack
+stack: Stack:Head-><-[0]-> |||
+
+stack: Stack:Head-><-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[20]-> <-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+stack: Stack:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+
+
+it->>90
+it->>80
+it->>70
+it->>60
+it->>50
+it->>40
+it->>30
+it->>20
+it->>10
+it->>0
+
+
+popping stack: [  90]: Stack:Head-><-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+popping stack: [  80]: Stack:Head-><-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+popping stack: [  70]: Stack:Head-><-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+popping stack: [  60]: Stack:Head-><-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+popping stack: [  50]: Stack:Head-><-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+popping stack: [  40]: Stack:Head-><-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+popping stack: [  30]: Stack:Head-><-[20]-> <-[10]-> <-[0]-> |||
+
+popping stack: [  20]: Stack:Head-><-[10]-> <-[0]-> |||
+
+popping stack: [  10]: Stack:Head-><-[0]-> |||
+
+popping stack: [   0]: Stack:Head->|||
+
+
+
+Iterator is_null(): true
+
+----------------------- 
+
+[       OK ] TEST_FUNCTIONS.TestStack (1 ms)
+[----------] 3 tests from TEST_FUNCTIONS (1 ms total)
+
+[----------] Global test environment tear-down
+[==========] 3 tests from 1 test case ran. (2 ms total)
+[  PASSED  ] 3 tests.
+08-stack-queue git:(master) ✗  😊 $> 
+*/
+
+
