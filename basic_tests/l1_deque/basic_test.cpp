12a5a55afe6053be03dd8e6203fcca0a1d625a65
barkeshli <sxbarkeshli@pasadena.edu>
2021-02-27 00:48:13 -0800

    deque basic test + output

--- basic_tests/l1_deque/basic_test.cpp
+++ basic_tests/l1_deque/basic_test.cpp
@@ -88,3 +88,302 @@ int main(int argc, char **argv) {
   return RUN_ALL_TESTS();
 }
 
+/*
+
+l1_dequeue git:(master) ✗  😊 $> tr includes
+includes
+├── deque
+├── linked_list_functions
+└── node
+
+3 directories, 0 files
+l1_dequeue git:(master) ✗  😊 $> ./build/bin/basic_test
+
+
+----------running basic_test.cpp---------
+
+
+[==========] Running 2 tests from 1 test case.
+[----------] Global test environment set-up.
+[----------] 2 tests from TEST_DEQUE
+[ RUN      ] TEST_DEQUE.TestDeque_rev
+Deque: Deque:Head-><-[0]-> |||
+
+Deque: Deque:Head-><-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+
+
+it->>90
+it->>80
+it->>70
+it->>60
+it->>50
+it->>40
+it->>30
+it->>20
+it->>10
+it->>0
+
+
+popping Deque: [   0]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> |||
+
+popping Deque: [  10]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> |||
+
+popping Deque: [  20]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> |||
+
+popping Deque: [  30]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> |||
+
+popping Deque: [  40]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> |||
+
+popping Deque: [  50]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> |||
+
+popping Deque: [  60]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> |||
+
+popping Deque: [  70]: Deque:Head-><-[90]-> <-[80]-> |||
+
+popping Deque: [  80]: Deque:Head-><-[90]-> |||
+
+popping Deque: [  90]: Deque:Head->|||
+
+
+
+Iterator is_null(): true
+
+----------------------- 
+
+[       OK ] TEST_DEQUE.TestDeque_rev (1 ms)
+[ RUN      ] TEST_DEQUE.TestDeque
+Deque: Deque:Head-><-[0]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+
+
+it->>0
+it->>10
+it->>20
+it->>30
+it->>40
+it->>50
+it->>60
+it->>70
+it->>80
+it->>90
+
+
+popping Deque: [   0]: Deque:Head-><-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  10]: Deque:Head-><-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  20]: Deque:Head-><-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  30]: Deque:Head-><-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  40]: Deque:Head-><-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  50]: Deque:Head-><-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  60]: Deque:Head-><-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  70]: Deque:Head-><-[80]-> <-[90]-> |||
+
+popping Deque: [  80]: Deque:Head-><-[90]-> |||
+
+popping Deque: [  90]: Deque:Head->|||
+
+
+
+Iterator is_null(): true
+
+----------------------- 
+
+[       OK ] TEST_DEQUE.TestDeque (2 ms)
+[----------] 2 tests from TEST_DEQUE (3 ms total)
+
+[----------] Global test environment tear-down
+[==========] 2 tests from 1 test case ran. (3 ms total)
+[  PASSED  ] 2 tests.
+l1_dequeue git:(master) ✗  😊 $> cd build              
+build git:(master) ✗  😊 $> make && ./bin/basic_test
+[ 10%] Built target gtest
+Scanning dependencies of target basic_test
+[ 15%] Building CXX object CMakeFiles/basic_test.dir/_tests/_test_files/basic_test.cpp.o
+[ 21%] Linking CXX executable bin/basic_test
+[ 21%] Built target basic_test
+[ 31%] Built target testA
+[ 42%] Built target testB
+[ 52%] Built target main
+[ 68%] Built target gmock
+[ 89%] Built target gmock_main
+[100%] Built target gtest_main
+
+
+----------running basic_test.cpp---------
+
+
+[==========] Running 2 tests from 1 test case.
+[----------] Global test environment set-up.
+[----------] 2 tests from TEST_DEQUE
+[ RUN      ] TEST_DEQUE.TestDeque_rev
+Deque: Deque:Head-><-[0]-> |||
+
+Deque: Deque:Head-><-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+Deque: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> <-[0]-> |||
+
+
+
+it->>90
+it->>80
+it->>70
+it->>60
+it->>50
+it->>40
+it->>30
+it->>20
+it->>10
+it->>0
+
+
+popping Deque: [   0]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> <-[10]-> |||
+
+popping Deque: [  10]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> <-[20]-> |||
+
+popping Deque: [  20]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> <-[30]-> |||
+
+popping Deque: [  30]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> <-[40]-> |||
+
+popping Deque: [  40]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> <-[50]-> |||
+
+popping Deque: [  50]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> <-[60]-> |||
+
+popping Deque: [  60]: Deque:Head-><-[90]-> <-[80]-> <-[70]-> |||
+
+popping Deque: [  70]: Deque:Head-><-[90]-> <-[80]-> |||
+
+popping Deque: [  80]: Deque:Head-><-[90]-> |||
+
+popping Deque: [  90]: Deque:Head->|||
+
+
+
+Iterator is_null(): true
+
+----------------------- 
+
+[       OK ] TEST_DEQUE.TestDeque_rev (1 ms)
+[ RUN      ] TEST_DEQUE.TestDeque
+Deque: Deque:Head-><-[0]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> |||
+
+Deque: Deque:Head-><-[0]-> <-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+
+
+it->>0
+it->>10
+it->>20
+it->>30
+it->>40
+it->>50
+it->>60
+it->>70
+it->>80
+it->>90
+
+
+popping Deque: [   0]: Deque:Head-><-[10]-> <-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  10]: Deque:Head-><-[20]-> <-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  20]: Deque:Head-><-[30]-> <-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  30]: Deque:Head-><-[40]-> <-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  40]: Deque:Head-><-[50]-> <-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  50]: Deque:Head-><-[60]-> <-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  60]: Deque:Head-><-[70]-> <-[80]-> <-[90]-> |||
+
+popping Deque: [  70]: Deque:Head-><-[80]-> <-[90]-> |||
+
+popping Deque: [  80]: Deque:Head-><-[90]-> |||
+
+popping Deque: [  90]: Deque:Head->|||
+
+
+
+Iterator is_null(): true
+
+----------------------- 
+
+[       OK ] TEST_DEQUE.TestDeque (0 ms)
+[----------] 2 tests from TEST_DEQUE (1 ms total)
+
+[----------] Global test environment tear-down
+[==========] 2 tests from 1 test case ran. (1 ms total)
+[  PASSED  ] 2 tests.
+build git:(master) ✗  😊 $> 
+*/
